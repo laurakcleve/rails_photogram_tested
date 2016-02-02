fcfccb0eb3ae43ab4ec74a2e6e5aff838fac1a4b
lrounin <lrounin@gmail.com>
2016-02-02 19:17:11 +0000

    Initial commit

---
+++ vendor/bundle/gems/debug_inspector-0.0.2/ext/debug_inspector/siteconf20160202-1366-431klp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160202-1366-1ddgadm"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/gems/json-1.8.3/ext/json/ext/generator/siteconf20160202-1366-o3ulwv.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160202-1366-q9d3oj"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
