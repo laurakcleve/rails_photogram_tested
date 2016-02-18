ec562b0601114fab079cbbe33fe5cb658e76e17d
lrounin <lrounin@gmail.com>
2016-02-18 23:04:31 +0000

    Try to fix create comment functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -19,7 +19,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content) if params[:status]
   end
 
   def set_post
